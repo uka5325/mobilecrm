44c9325c59423f6be5d7114444c3c31390fc0f07
uka5325 <uka5325@gmail.com>
2026-06-05 21:04:55 +0900

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -30,7 +30,3 @@ if (typeof window !== "undefined") {
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-console.log("FIREBASE PROJECT", {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-});
